fb68961df1644e7770cdc43d85d8686b7c35bbe4
Daet0ya <Darckas8@gmail.com>
2019-10-24 10:01:40 +0300

    lua-add

---
+++ lab01_illarionov.lua
@@ -0,0 +1,14 @@
+lgi = require 'lgi'
+
+gtk = lgi.Gtk
+gtk.init()
+
+bld = gtk.Builder()
+bld:add_from_file('lab01_illarionov.glade')
+
+ui = bld.objects
+ui.wnd.title = 'lab-01'
+ui.wnd.on_destroy = gtk.main_quit
+ui.wnd:show_all()
+
+gtk.main()
